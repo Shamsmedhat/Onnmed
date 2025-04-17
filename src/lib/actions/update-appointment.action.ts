f29703ff78ecce354262fa1e1f886dc89714c67a
Shamsmedhat <125091923+Shamsmedhat@users.noreply.github.com>
2025-04-18 01:43:56 +0200

    Update update-appointment.action.ts

--- src/lib/actions/update-appointment.action.ts
+++ src/lib/actions/update-appointment.action.ts
@@ -6,13 +6,14 @@ import { revalidateTag } from "next/cache";
 import { cookies } from "next/headers";
 
 export async function updateAppointmentAction(fields: { status: string }, id: string) {
-  const tokenCookie2 = cookies().get("next-auth.session-token");
+
+  cookies().set("test", "public");
+  const allCookies = cookies().toString();
+  console.log("allCookies", allCookies);
+  
   const tokenCookie = cookies().get("next-auth.session-token")?.value;
   const token = await decode({ secret: process.env.NEXTAUTH_SECRET!, token: tokenCookie });
-  console.log(tokenCookie2)
-  console.log(tokenCookie)
-  console.log({ ...fields, userType: token?.userType })
-  console.log(process.env.NEXTAUTH_SECRET!)
+
   const res = await fetch(`${process.env.NEXT_PUBLIC_API}/api/appointments/${id}`, {
     method: "PUT",
     headers: {
